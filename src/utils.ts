c3faf3980557804d4f7ad940f14e040e0c7cde0a
caijf <369756941@qq.com>
2023-05-29 17:36:37 +0800

    feat: demo示例

---
+++ src/utils.ts
@@ -0,0 +1,17 @@
+// 获取字符串的 unicode 编码
+// '安'.charCodeAt(); // 23433 注意输出的是10进制
+// String.fromCharCode(23433); // 安
+
+// 如果要使用 \u 表示法，就需要使用 toString 转 16进制，再处理。
+
+
+// unicode 转字符串
+export function unicodeToString(code: string) {
+  return JSON.parse(`"\\u${code}"`);
+}
+
+// 字符串转 unicode
+export function stringToUnicode(str: string) {
+  return str.charCodeAt(0).toString(16);
+}
+
